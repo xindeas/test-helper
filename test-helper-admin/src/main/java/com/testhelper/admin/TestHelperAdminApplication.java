21c44a3861c01d0fa066ec0aabb4c224866ffa6e
xindeas <1810968463@qq.com>
2021-08-12 11:53:18 +0800

    添加扫描

--- test-helper-admin/src/main/java/com/testhelper/admin/TestHelperAdminApplication.java
+++ test-helper-admin/src/main/java/com/testhelper/admin/TestHelperAdminApplication.java
@@ -2,8 +2,10 @@ package com.testhelper.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.testhelper.generator"})
 public class TestHelperAdminApplication {
 
     public static void main(String[] args) {
